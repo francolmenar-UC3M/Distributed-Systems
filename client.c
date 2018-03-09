26d999b110a5d4933ca3b6a7d8580db14ca3aa9a
francolmenar <correofalso97@hotmail.es>
2018-03-09 19:46:24 +0000

    joder

--- client.c
+++ client.c
@@ -1,56 +1,58 @@
 #include "mensaje.h"
 #include "keys.c"
+#include <unistd.h>
 
-#define NCLIENTS 2 /*number f threads to be created*/
+#define NCLIENTS 1 /*number f threads to be created*/
 
 /*It simulates a common execution of a client*/
 static int normalExe(){
   if( (init()) < 0 ) /*init de database*/
   {
-    perror("Error while init (CLIENT)");
-    return -1;
+    printf("Error while init (CLIENT)\n");
+    sleep(1);
   }
 
   printf("I set 1, 2 and 3\n"); /*adding new elements*/
   if( (set_value(1, "Uno", 1.0)) < 0 )
   {
-    perror("Error while set (CLIENT)");
-    return -1;
-  }
-  if( (set_value(2, "Dos", 2.0)) < 0 )
-  {
-    perror("Error while set (CLIENT)");
-    return -1;
-  }
-  if( (set_value(3, "Tres", 3.0)) < 0 )
-  {
-    perror("Error while set (CLIENT)");
-    return -1;
+    printf("Error while set (CLIENT)\n");
+    sleep(10);
   }
 
   char aux [MAXSIZE]; /*getting a value of the server*/
   float aux2 = 40.0;
-  printf("I get the values of 2\n");
-  if( (get_value(2,aux, &aux2)) < 0 )
+  printf("I get the values of 1\n");
+  if( (get_value(1,aux, &aux2)) < 0 )
   {
-    perror("Error while get (CLIENT)");
-    return -1;
+    printf("Error while get (CLIENT)\n");
+    sleep(1);
   }
-  printf("Values of 2: value1 = %s\tvalue2 = %f\n",aux, aux2);
+  printf("Values of 1: value1 = %s\tvalue2 = %f\n",aux, aux2);
 
-  aux2 = 4; /*modify a value*/
+  if( (set_value(2, "Dos", 2.0)) < 0 )
+  {
+    printf("Error while set (CLIENT)\n");
+    sleep(1);
+  }
+  if( (set_value(3, "Tres", 3.0)) < 0 )
+  {
+    printf("Error while set (CLIENT)\n");
+    sleep(1);
+  }
+
+  aux2 = 4.0; /*modify a value*/
   printf("I modify the values of 3\n");
   if( (modify_value(3, aux, &aux2)) < 0 )
   {
-    perror("Error while modify (CLIENT)");
-    return -1;
+    printf("Error while modify (CLIENT)\n");
+    sleep(1);
   }
 
   printf("I get the modified values\n");
   if( (get_value(3,aux, &aux2)) < 0 ) /*get the modified value*/
   {
-    perror("Error while get (CLIENT)");
-    return -1;
+    printf("Error while get (CLIENT)\n");
+    sleep(1);
   }
   printf("Values of 3 after modification: value1 = %s\tvalue2 = %f\n",aux, aux2);
 
@@ -58,15 +60,15 @@ static int normalExe(){
   printf("I delete 1\n"); /*delete an element of the server*/
   if( (delete_key(1)) < 0 )
   {
-    perror("Error while delete (CLIENT)");
-    return -1;
+    printf("Error while delete (CLIENT)\n");
+    sleep(1);
   }
 
   int n = num_items(); /*getting the amount of elements in the server*/
   if( (n) < 0 )
   {
-    perror("Error while   (CLIENT)");
-    return -1;
+    printf("Error while   (CLIENT)\n");
+    sleep(1);
   }
   printf("Number of elements: %i\n", n);
   return 0;
