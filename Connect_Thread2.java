d8edb4c0d5bc18ddb7ce4b1f9b4f917cc29652cc
francolmenar <100346094@alumnos.uc3m.es>
2018-05-14 19:01:06 +0200

    Connect_Thread2

---
+++ Connect_Thread2.java
@@ -0,0 +1,135 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Connect_Thread2 extends Thread {
+    private static final String SEND_MESSAGE = "SEND_MESSAGE";
+    private static final String SEND_ATTACH = "SEND_ATTACH";
+
+    private static ServerSocket serverSocket; // socket to listen to the server
+
+    public Connect_Thread(ServerSocket serverSocket){
+        this.serverSocket = serverSocket;
+    }
+
+    public void run(){
+        if(!clientPart2.checkNullParameters(new ServerSocket[]{serverSocket},Thread.currentThread().getStackTrace()[1].getMethodName())){return;} // check for the validity of the parameters
+
+        String operation; // the operation to be performed
+        Socket clientSocket = new Socket();
+        while(!interrupted()){ // Executed while the thread is not interrupted
+            try {
+                clientSocket = serverSocket.accept(); // accept the connection
+            } catch (IOException e) {
+                return;
+                // Thread finished
+            }
+            DataInputStream input  = null; // buffer reader
+
+            try {
+                input = new DataInputStream(clientSocket.getInputStream());
+            } catch (IOException e) {
+                System.out.print("Server cannot connect to the client\nc> ");
+                return;
+            }
+
+            if((operation = clientPart2.receiveString(input)) == null) { // get the operation to be performed
+                System.out.print("Error receiving a message from the server\nc> ");
+                closeSocket(clientSocket);
+                break;
+            }
+
+            /* ********** SEND MESSAGE ************ */
+            if(operation.equals(SEND_MESSAGE)){
+                if(!sendOperation(input)){ // Execute send message operation
+                    System.out.print("Error receiving a message from the server\nc> ");
+                    closeSocket(clientSocket);
+                    break;
+                }
+            }
+
+            /* ********** SEND MESSAGE ************ */
+            else if(operation.equals(SEND_ATTACH)){
+                if(!sendFileOperationn(input)){ // Execute send message operation
+                    System.out.print("Error receiving a message from the server\nc> ");
+                    closeSocket(clientSocket);
+                    break;
+                }
+            }
+            else{
+                System.out.print("Error receiving a message from the server\nc> ");
+            }
+            closeSocket(clientSocket);
+        }
+    }
+
+    private void closeSocket(Socket clientSocket) {
+        try {
+            clientSocket.close();
+        } catch (IOException e) {
+            System.out.println("Error receiving a message from the server");
+        }
+    }
+
+    /**
+     * @brief Receives a message from the client
+     *
+     * @param input: the input channel to read the String sent by the server
+     */
+    private boolean sendOperation(DataInputStream input){
+        if(!clientPart2.checkNullParameters(new DataInputStream[]{input},Thread.currentThread().getStackTrace()[1].getMethodName())){return false;} // check for the validity of the parameters
+
+        String originUser, msgId, msg; // Strings to be received from the server
+        if((originUser = clientPart2.receiveString(input)) == null){ // Read the originUser
+            System.out.print("Error receiving a message from the server\nc> ");
+            return false;
+        }
+        else if((msgId = clientPart2.receiveString(input)) == null){ // Read the msgId
+            System.out.print("Error receiving a message from the server\nc> ");
+            return false;
+        }
+        else if((msg = clientPart2.receiveString(input)) == null){ // Read the msg
+            System.out.print("Error receiving a message from the server\nc> ");
+            return false;
+        }
+        System.out.print("MESSAGE " + msgId + " FROM " + originUser + ":\n" + "   " + msg + "\n   END\nc> ");
+        return true;
+    }
+
+    /**
+     * @brief Receives a message with a file from the client
+     *
+     * @param input: the input channel to read the String sent by the server
+     */
+    private boolean sendFileOperationn(DataInputStream input) {
+        if(!clientPart2.checkNullParameters(new DataInputStream[]{input},Thread.currentThread().getStackTrace()[1].getMethodName())){return false;} // check for the validity of the parameters
+
+        String originUser, msgId, msg, fileName, fileContent; // Strings to be received from the server
+        if((originUser = clientPart2.receiveString(input)) == null){ // Read the originUser
+            System.out.print("Error receiving a message from the server\nc> ");
+            return false;
+        }
+        else if((msgId = clientPart2.receiveString(input)) == null){ // Read the msgId
+            System.out.print("Error receiving a message from the server\nc> ");
+            return false;
+        }
+        else if((msg = clientPart2.receiveString(input)) == null){ // Read the msg
+            System.out.print("Error receiving a message from the server\nc> ");
+            return false;
+        }
+
+        else if((fileName = clientPart2.receiveString(input)) == null){ // Read the FileNamw
+            System.out.print("Error receiving a message from the server\nc> ");
+            return false;
+        }
+
+        else if((fileContent = clientPart2.receiveString(input)) == null){ // Read the FileNamw
+            System.out.print("Error receiving a message from the server\nc> ");
+            return false;
+        }
+        System.out.print("MESSAGE " + msgId + " FROM " + originUser + ":\n" + "   " + msg +
+                "\n   File Name: " + fileName + "\n" + fileContent + "\n   END\nc> ");
+        return true;
+    }
+}
